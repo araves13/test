bd6fb0afcfe41560c2d9285bee6d3ccca75acb67
Abdullaeva Sevara <33462559+araves13@users.noreply.github.com>
2022-09-04 12:06:30 +0000

    Create problem_3.cpp

---
+++ problem_3.cpp
@@ -0,0 +1,10 @@
+//Write C++ program that takes the temperature in Celsius and converts it to Fahrenheit. Print the result of the problem
+#include <iostream> //enables program to perform input output
+using namespace std;
+
+int main() {
+    //write your code here
+
+
+    return 0;
+}
