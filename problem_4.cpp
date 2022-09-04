550da5f4120723fffc156ad2270d346b5a8da9eb
Abdullaeva Sevara <33462559+araves13@users.noreply.github.com>
2022-09-04 12:07:37 +0000

    Create problem_4.cpp

---
+++ problem_4.cpp
@@ -0,0 +1,11 @@
+//Write C++ program for the following problem. You have N number of students in the course. For the Computer Programming Lab you need to divide them into groups. One group can accommodate 24 students as there are only 24 computers in the lab class. Calculate how many computers will stay free when the last group comes to lab class? Take N as the user input.
+
+#include <iostream> //enables program to perform input output
+using namespace std;
+
+int main() {
+    //write your code here
+
+
+    return 0;
+}
