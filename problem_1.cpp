6805960dc404c35d82f1fd1eedced322555fe1ac
Abdullaeva Sevara <33462559+araves13@users.noreply.github.com>
2022-09-04 12:03:44 +0000

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,10 +1,9 @@
-%Write C++ program that takes two numbers(int) from user and calculates sum and product of
-them.Print the result of summation and multiplication operations.
+//Write C++ program that takes two numbers(int) from user and calculates sum and product of them.Print the result of summation and multiplication operations.
 
 #include <iostream> //enables program to perform input output
 
 int main() {
-    %write your code here
+    //write your code here
 
 
     return 0;
